bae76fda053a1e09474053dd4a795ee072179f89
David Rheinsberg <david@readahead.eu>
2025-05-09 22:45:08 +0200

    sys: introduce system library
    Introduce the system-dependent counterpart to `osi`. This is a stub, but
    will be filled in the following commits.
    
    Signed-off-by: David Rheinsberg <david@readahead.eu>

---
+++ lib/sys/src/lib.rs
@@ -0,0 +1,13 @@
+//! # Capability-based System Interfaces
+//!
+//! This library provides system interfaces following a capability-based
+//! design. It does not require any particular runtime, but can optionally
+//! be combined with the Rust Standard Library.
+
+#![no_std]
+
+extern crate alloc;
+extern crate core;
+
+#[cfg(any(test, feature = "std"))]
+extern crate std;
