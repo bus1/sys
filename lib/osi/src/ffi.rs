bf7d1ef908308cbafcbc2d8286099761b5114991
David Rheinsberg <david@readahead.eu>
2025-05-09 21:51:32 +0200

    osi/ffi: expose more endian helpers
    Ensure all public endian API helpers are exposed via the ffi module.
    
    Signed-off-by: David Rheinsberg <david@readahead.eu>

--- lib/osi/src/ffi.rs
+++ lib/osi/src/ffi.rs
@@ -23,7 +23,15 @@ pub mod integer;
 pub mod packed;
 pub mod pointer;
 
-pub use endian::{BigEndian, LittleEndian, NativeEndian};
+pub use endian::{
+    BigEndian,
+    from_native,
+    from_raw,
+    LittleEndian,
+    NativeEndian,
+    to_native,
+    to_raw,
+};
 pub use integer::Integer;
 pub use packed::Packed;
 pub use pointer::{NativeAddress, Pointer};
