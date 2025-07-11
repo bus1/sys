6e85d0bfbdba37dcb466a2c32c71451a7f3d1b71
David Rheinsberg <david@readahead.eu>
2025-07-11 13:06:19 +0200

    osi/meta: simplify Field trait
    Simplify the `Field` trait and extend it to support ZSTs. Similarly
    adjust the `PinnedField` trait to reflect those changes.
    
    Signed-off-by: David Rheinsberg <david@readahead.eu>

--- lib/osi/src/meta.rs
+++ lib/osi/src/meta.rs
@@ -3,59 +3,33 @@
 //! This module provides utilities for meta programming, including (limited)
 //! runtime type information, type introspection, or even reflection.
 
-/// A view into a container for a specific member field.
-///
-/// The `FieldView` type wraps a reference to a member field of a container,
-/// granting access to both the member field as well as the container.
-///
-/// Views are usually created via the [`Field`] trait.
-#[repr(transparent)]
-pub struct FieldView<'this, Container, const OFFSET: usize>
-where
-    Container: Field<OFFSET>,
-{
-    field: &'this Container::Value,
-}
-
-/// A mutable view into a container for a specific member field.
-///
-/// The `FieldViewMut` type wraps a mutable reference to a member field of a
-/// container, granting access to both the member field as well as the
-/// container.
-///
-/// Views are usually created via the [`Field`] trait.
-#[repr(transparent)]
-pub struct FieldViewMut<'this, Container, const OFFSET: usize>
-where
-    Container: Field<OFFSET>,
-{
-    field: &'this mut Container::Value,
-}
-
 /// Grant generic access to member fields.
 ///
 /// This trait generalizes over member fields of structures. It allows granting
 /// access to a specific field without requiring any other knowledge about the
 /// containing type.
 ///
-/// Any type that implements `Field<OFFSET>` is guaranteed to have a field
-/// member at relative offset `OFFSET`. The type of the field member is given
-/// as `Field<OFFSET>::Value`.
+/// Any type that implements `Field<OFFSET, T>` is guaranteed to have a field
+/// member at relative offset `OFFSET`. The type of the field member is `T`.
 ///
-/// Note that if multiple member fields are zero-sized and have the same
-/// offset, this trait can only be implemented for one of them.
+/// Note that multiple fields might share an offset if at least on of them
+/// is a zero-sized type.
 ///
 /// ## Safety
 ///
 /// Any implementation must guarantee that the implementing type has a member
-/// field of type `Self::Value` at offset `OFFSET` relative to its starting
-/// address. The member field must follow standard alignment and sizing
-/// requirements.
+/// field of type `T` at offset `OFFSET` relative to its starting address. The
+/// member field must follow standard alignment and sizing requirements.
+///
+/// The member field must be a direct member of the structure (rather than
+/// nested in sub-structures). In particular, its type must match exactly the
+/// type `T`.
 ///
 /// ## Example
 ///
 /// ```rust
 /// use core::mem::offset_of;
+/// use osi::mem::typed_offset_of;
 /// use osi::meta::Field;
 ///
 /// struct Position {
@@ -63,53 +37,29 @@ where
 ///     y: u64,
 /// }
 ///
-/// unsafe impl Field<{offset_of!(Position, x)}> for Position {
-///     type Value = u32;
+/// unsafe impl Field<{typed_offset_of!(Position, x, u32)}, u32> for Position {
 /// }
 ///
-/// unsafe impl Field<{offset_of!(Position, y)}> for Position {
-///     type Value = u64;
+/// unsafe impl Field<{typed_offset_of!(Position, y, u64)}, u64> for Position {
 /// }
 ///
 /// let pos = Position { x: 11, y: 1444 };
-/// let pos_x = <_ as Field<{offset_of!{Position, x}}>>::field(&pos);
-/// let pos_y = <_ as Field<{offset_of!{Position, y}}>>::field(&pos);
+/// let pos_x = <_ as Field<{offset_of!{Position, x}}, _>>::field_of(&pos);
+/// let pos_y = <_ as Field<{offset_of!{Position, y}}, _>>::field_of(&pos);
 /// assert_eq!(*pos_x, pos.x);
 /// assert_eq!(*pos_y, pos.y);
 /// ```
-pub unsafe trait Field<const OFFSET: usize>
-where
-    Self: Sized,
-{
-    type Value;
-
+pub unsafe trait Field<const OFFSET: usize, T>: Sized {
     /// Turn a container reference into a member field reference.
-    fn field_of(container: &Self) -> &Self::Value {
+    fn field_of(container: &Self) -> &T {
         // SAFETY: The trait guarantees that there is a valid member field
         //         at the specified offset. Since we have the entire container
         //         borrowed, we can safely hand out sub-borrows.
         unsafe {
             &*(
                 (container as *const Self)
-                    .cast::<u8>()
-                    .offset(OFFSET as isize)
-                    .cast::<Self::Value>()
-            )
-        }
-    }
-
-    /// Turn a mutable container reference into a mutable member field
-    /// reference.
-    fn field_of_mut(container: &mut Self) -> &mut Self::Value {
-        // SAFETY: The trait guarantees that there is a valid member field
-        //         at the specified offset. Since we have the entire container
-        //         borrowed, we can safely hand out sub-borrows.
-        unsafe {
-            &mut *(
-                (container as *mut Self)
-                    .cast::<u8>()
-                    .offset(OFFSET as isize)
-                    .cast::<Self::Value>()
+                    .byte_offset(OFFSET as isize)
+                    .cast::<T>()
             )
         }
     }
@@ -124,173 +74,15 @@ where
     ///
     /// Note that these guarantees are always given if `field` was acquired
     /// via `Self::field_of()`.
-    unsafe fn container_of(field: &Self::Value) -> &Self {
-        // SAFETY: The caller must guarantee that `field` was acquired via
-        //         `field_of()`, hence we know that the entire container is
-        //         borrowed. Thus it is safe to reverse the operation.
+    unsafe fn container_of(field: &T) -> &Self {
+        // SAFETY: The caller must guarantee that `field` was borrowed
+        //         together with the entire container.
         unsafe {
             &*(
-                (field as *const Self::Value)
-                    .cast::<u8>()
-                    .offset(OFFSET as isize)
+                (field as *const T)
+                    .byte_offset(-(OFFSET as isize))
                     .cast::<Self>()
             )
-        }
-    }
-
-    /// Turn a mutable member field reference back into a mutable container
-    /// reference.
-    ///
-    /// ## Safety
-    ///
-    /// The caller must guarantee that `field` points to a member field of a
-    /// container of type `Self`. Furthermore, the entire container must be
-    /// mutably borrowed for at least the lifetime of `field`.
-    ///
-    /// Note that these guarantees are always given if `field` was acquired
-    /// via `Self::field_of_mut()`.
-    unsafe fn container_of_mut(field: &mut Self::Value) -> &mut Self {
-        // SAFETY: The caller must guarantee that `field` was acquired via
-        //         `field_of_mut()`, hence we know that the entire container is
-        //         mutably borrowed. Thus it is safe to reverse the operation.
-        unsafe {
-            &mut *(
-                (field as *mut Self::Value)
-                    .cast::<u8>()
-                    .offset(OFFSET as isize)
-                    .cast::<Self>()
-            )
-        }
-    }
-
-    /// Return a reference to the member field of the container.
-    fn field(&self) -> &Self::Value {
-        Self::field_of(self)
-    }
-
-    /// Return a mutable reference to the member field of the container.
-    fn field_mut(&mut self) -> &mut Self::Value {
-        Self::field_of_mut(self)
-    }
-
-    /// Provide a view to the member field of the container.
-    fn view(&self) -> FieldView<'_, Self, OFFSET> {
-        FieldView::with_container(self)
-    }
-
-    /// Provide a mutable view to the member field of the container.
-    fn view_mut(&mut self) -> FieldViewMut<'_, Self, OFFSET> {
-        FieldViewMut::with_container(self)
-    }
-}
-
-impl<'this, Container, const OFFSET: usize>
-    FieldView<'this, Container, OFFSET>
-where
-    Container: Field<OFFSET>,
-{
-    /// Create a new field view from the container.
-    ///
-    /// This takes a container by reference, and creates a view to the member
-    /// field at offset `OFFSET`. This view allows accessing the member field,
-    /// but also borrowing the container again.
-    pub fn with_container(container: &'this Container) -> Self {
-        Self {
-            field: Field::field_of(container),
-        }
-    }
-
-    /// Get a reference to the member field.
-    pub fn field(&self) -> &'this Container::Value {
-        self.field
-    }
-
-    /// Get a reference to the container.
-    pub fn container(&self) -> &'this Container {
-        // SAFETY: We know `self.field` was gained via `Field::field_of()`,
-        //         hence we know the entire container is borrowed. This is
-        //         required for `container_of()`.
-        unsafe {
-            Field::container_of(self.field)
-        }
-    }
-
-    /// Turn this into a reference to the member field.
-    pub fn into_ref(self) -> &'this Container::Value {
-        self.field
-    }
-
-    /// Turn this back into a reference to the container.
-    pub fn into_container(self) -> &'this Container {
-        // SAFETY: We know `self.field` was gained via `Field::field_of()`,
-        //         hence we know the entire container is borrowed. This is
-        //         required for `container_of()`.
-        unsafe {
-            Field::container_of(self.field)
-        }
-    }
-}
-
-impl<'this, Container, const OFFSET: usize>
-    FieldViewMut<'this, Container, OFFSET>
-where
-    Container: Field<OFFSET>,
-{
-    /// Create a new mutable field view from the container.
-    ///
-    /// This takes a container by mutable reference, and creates a mutable view
-    /// to the member field at offset `OFFSET`. This view allows access to both
-    /// the member field and the container.
-    pub fn with_container(container: &'this mut Container) -> Self {
-        Self {
-            field: Field::field_of_mut(container),
-        }
-    }
-
-    /// Get a reference to the member field.
-    pub fn field(&self) -> &Container::Value {
-        self.field
-    }
-
-    /// Get a mutable reference to the member field.
-    pub fn field_mut(&mut self) -> &mut Container::Value {
-        self.field
-    }
-
-    /// Get a reference to the container.
-    pub fn container(&self) -> &Container {
-        // SAFETY: We know `self.field` was gained via `Field::field_of_mut()`,
-        //         hence we know the entire container is mutably borrowed. This
-        //         is enough for `container_of()`.
-        unsafe {
-            Field::container_of(self.field)
-        }
-    }
-
-    /// Get a mutable reference to the container.
-    pub fn container_mut(&mut self) -> &mut Container {
-        // SAFETY: We know `self.field` was gained via `Field::field_of_mut()`,
-        //         hence we know the entire container is mutably borrowed. This
-        //         is required for `container_of_mut()`.
-        unsafe {
-            Field::container_of_mut(self.field)
-        }
-    }
-
-    /// Turn this into a reference to the member field.
-    pub fn into_ref(self) -> &'this Container::Value {
-        self.field
-    }
-
-    /// Turn this into a mutable reference to the member field.
-    pub fn into_mut(self) -> &'this mut Container::Value {
-        self.field
-    }
-
-    /// Turn this back into a reference to the container.
-    pub fn into_container(self) -> &'this mut Container {
-        unsafe {
-            Field::container_of_mut(self.field)
         }
     }
 }
@@ -298,26 +90,29 @@ where
 #[cfg(test)]
 mod test {
     use super::*;
+    use crate::mem;
 
-    #[derive(Default)]
+    #[derive(Clone, Copy, Debug, Default, PartialEq)]
     #[repr(C, align(4))]
     struct Test {
-        a: u32,
+        a: u16,
         b: u8,
         c: u32,
     }
 
-    unsafe impl Field<{core::mem::offset_of!(Test, b)}> for Test {
-        type Value = u8;
+    unsafe impl Field<{mem::typed_offset_of!(Test, b, u8)}, u8> for Test {
     }
 
     // Basic functionality tests for `Field`.
     #[test]
     fn basic_field() {
-        assert_eq!(core::mem::size_of::<Test>(), 12);
+        assert_eq!(core::mem::size_of::<Test>(), 8);
 
-        let t = Test { a: 14, b: 11, c: 1444 };
-        let v = t.view();
-        assert_eq!(*v.field(), 11);
+        let c = Test { a: 14, b: 11, c: 1444 };
+        let f = Field::field_of(&c);
+
+        assert_eq!(*f, 11);
+        assert!(core::ptr::eq(&c, unsafe { Field::container_of(f) }));
+        assert_eq!(c, unsafe { *Field::container_of(f) });
     }
 }
--- lib/osi/src/pin.rs
+++ lib/osi/src/pin.rs
@@ -1,63 +1,30 @@
 //! # Pin Utilities
 //!
 //! This module provides utilities to simplify use of [`core::pin::Pin`], as
-//! well as provide pinned alternatives for the other APIs provide by this
-//! crate.
+//! well as pinned alternatives for traits defined elsewhere.
 
 use core::pin;
-use crate::meta;
-
-/// A pinned view into a container for a specific member field.
-///
-/// This trait is a pinned alternative to [`meta::FieldView`].
-#[repr(transparent)]
-pub struct PinnedFieldView<'this, Container, const OFFSET: usize>
-where
-    Container: PinnedField<OFFSET>,
-{
-    field: pin::Pin<&'this Container::Value>,
-}
-
-/// A pinned mutable view into a container for a specific member field.
-///
-/// This trait is a pinned alternative to [`meta::FieldViewMut`].
-#[repr(transparent)]
-pub struct PinnedFieldViewMut<'this, Container, const OFFSET: usize>
-where
-    Container: PinnedField<OFFSET>,
-{
-    field: pin::Pin<&'this mut Container::Value>,
-}
 
 /// Grant generic access to pinned member fields.
 ///
-/// This trait is a pinned alternative to [`meta::Field`].
+/// This trait is a pinned alternative to [`crate::meta::Field`].
 ///
 /// ## Safety
 ///
-/// Any implementation must uphold the requirements of [`meta::Field`]. On top,
-/// the member field at offset `OFFSET` must follow the rules of
+/// Any implementation must uphold the requirements of [`crate::meta::Field`].
+/// On top, the member field at offset `OFFSET` must follow the rules of
 /// `structural pinning` as described in [`core::pin::Pin`]
-pub unsafe trait PinnedField<const OFFSET: usize>
-where
-    Self: Sized + meta::Field<OFFSET>,
-{
+pub unsafe trait PinnedField<const OFFSET: usize, T>: Sized {
     /// Turn a pinned container reference into a pinned member field reference.
-    fn pinned_field_of(container: pin::Pin<&Self>) -> pin::Pin<&Self::Value> {
+    fn pinned_field_of(container: pin::Pin<&Self>) -> pin::Pin<&T> {
         // SAFETY: The trait guarantees structural pinning for the member
         //         field. Therefore, we can safely pin the field.
         unsafe {
-            container.map_unchecked(Self::field_of)
-        }
-    }
-
-    /// Turn a pinned mutable container reference into a pinned mutable member
-    /// field reference.
-    fn pinned_field_of_mut(container: pin::Pin<&mut Self>) -> pin::Pin<&mut Self::Value> {
-        // SAFETY: The trait guarantees structural pinning for the member
-        //         field. Therefore, we can safely pin the field.
-        unsafe {
-            container.map_unchecked_mut(Self::field_of_mut)
+            container.map_unchecked(|v| &*(
+                (v as *const Self)
+                    .byte_offset(OFFSET as isize)
+                    .cast::<T>()
+            ))
         }
     }
 
@@ -72,164 +39,15 @@ where
     ///
     /// Note that these guarantees are always given if `field` was acquired
     /// via `Self::pinned_field_of()`.
-    unsafe fn pinned_container_of(field: pin::Pin<&Self::Value>) -> pin::Pin<&Self> {
+    unsafe fn pinned_container_of(field: pin::Pin<&T>) -> pin::Pin<&Self> {
         // SAFETY: The caller guarantees that the entire container is borrowed
         //         and pinned.
         unsafe {
-            field.map_unchecked(|v| Self::container_of(v))
-        }
-    }
-
-    /// Turn a pinned mutable member field reference back into a pinned mutable
-    /// container reference.
-    ///
-    /// ## Safety
-    ///
-    /// The caller must guarantee that `field` points to a member field of a
-    /// container of type `Self`. Furthermore, the entire container must be
-    /// mutably borrowed and pinned for at least the lifetime of `field`.
-    ///
-    /// Note that these guarantees are always given if `field` was acquired
-    /// via `Self::pinned_field_of_mut()`.
-    unsafe fn pinned_container_of_mut(field: pin::Pin<&mut Self::Value>) -> pin::Pin<&mut Self> {
-        // SAFETY: The caller guarantees that the entire container is mutably
-        //         borrowed and pinned.
-        unsafe {
-            field.map_unchecked_mut(|v| Self::container_of_mut(v))
-        }
-    }
-
-    /// Return a pinned reference to the member field of the container.
-    fn pinned_field(self: pin::Pin<&Self>) -> pin::Pin<&Self::Value> {
-        Self::pinned_field_of(self)
-    }
-
-    /// Return a pinned mutable reference to the member field of the container.
-    fn pinned_field_mut(self: pin::Pin<&mut Self>) -> pin::Pin<&mut Self::Value> {
-        Self::pinned_field_of_mut(self)
-    }
-
-    /// Provide a pinned view to the member field of the container.
-    fn pinned_view(self: pin::Pin<&Self>) -> PinnedFieldView<'_, Self, OFFSET> {
-        PinnedFieldView::with_container(self)
-    }
-
-    /// Provide a pinned mutable view to the member field of the container.
-    fn pinned_view_mut(self: pin::Pin<&mut Self>) -> PinnedFieldViewMut<'_, Self, OFFSET> {
-        PinnedFieldViewMut::with_container(self)
-    }
-}
-
-impl<'this, Container, const OFFSET: usize>
-    PinnedFieldView<'this, Container, OFFSET>
-where
-    Container: PinnedField<OFFSET>,
-{
-    /// Create a new pinned field view from the container.
-    ///
-    /// This takes a container by pinned reference, and creates a pinned view
-    /// to the member field at offset `OFFSET`. This view allows accessing the
-    /// member field, but also borrowing the container again.
-    pub fn with_container(container: pin::Pin<&'this Container>) -> Self {
-        Self {
-            field: PinnedField::pinned_field_of(container),
-        }
-    }
-
-    /// Get a pinned reference to the member field.
-    pub fn field(&self) -> pin::Pin<&Container::Value> {
-        self.field
-    }
-
-    /// Get a reference to the container.
-    pub fn container(&self) -> pin::Pin<&Container> {
-        // SAFETY: We know `self.field` was gained via
-        //         `PinnedField::pinned_field_of()`, hence we know the entire
-        //         container is borrowed.
-        unsafe {
-            PinnedField::pinned_container_of(self.field)
-        }
-    }
-
-    /// Turn this into a pinned reference to the member field.
-    pub fn into_ref(self) -> pin::Pin<&'this Container::Value> {
-        self.field
-    }
-
-    /// Turn this back into a pinned reference to the container.
-    pub fn into_container(self) -> pin::Pin<&'this Container> {
-        // SAFETY: We know `self.field` was gained via `Field::field_of()`,
-        //         hence we know the entire container is borrowed. This is
-        //         required for `container_of()`.
-        unsafe {
-            PinnedField::pinned_container_of(self.field)
-        }
-    }
-}
-
-impl<'this, Container, const OFFSET: usize>
-    PinnedFieldViewMut<'this, Container, OFFSET>
-where
-    Container: PinnedField<OFFSET>,
-{
-    /// Create a new pinned mutable field view from the container.
-    ///
-    /// This takes a container by pinned mutable reference, and creates a
-    /// pinned mutable view to the member field at offset `OFFSET`. This view
-    /// allows access to both the member field and the container.
-    pub fn with_container(container: pin::Pin<&'this mut Container>) -> Self {
-        Self {
-            field: PinnedField::pinned_field_of_mut(container),
-        }
-    }
-
-    /// Get a pinned reference to the member field.
-    pub fn field(&self) -> pin::Pin<&Container::Value> {
-        self.field.as_ref()
-    }
-
-    /// Get a pinned mutable reference to the member field.
-    pub fn field_mut(&mut self) -> pin::Pin<&mut Container::Value> {
-        self.field.as_mut()
-    }
-
-    /// Get a pinned reference to the container.
-    pub fn container(&self) -> pin::Pin<&Container> {
-        // SAFETY: We know `self.field` was gained via
-        //         `PinnedField::pinned_field_of_mut()`, hence we know the
-        //         entire container is mutably borrowed and pinned.
-        unsafe {
-            PinnedField::pinned_container_of(self.field.as_ref())
-        }
-    }
-
-    /// Get a pinned mutable reference to the container.
-    pub fn container_mut(&mut self) -> pin::Pin<&mut Container> {
-        // SAFETY: We know `self.field` was gained via
-        //         `PinnedField::pinned_field_of_mut()`, hence we know the
-        //         entire container is mutably borrowed and pinned.
-        unsafe {
-            PinnedField::pinned_container_of_mut(self.field.as_mut())
-        }
-    }
-
-    /// Turn this into a pinned reference to the member field.
-    pub fn into_ref(self) -> pin::Pin<&'this Container::Value> {
-        self.field.into_ref()
-    }
-
-    /// Turn this into a pinned mutable reference to the member field.
-    pub fn into_mut(self) -> pin::Pin<&'this mut Container::Value> {
-        self.field
-    }
-
-    /// Turn this back into a pinned reference to the container.
-    pub fn into_container(self) -> pin::Pin<&'this mut Container> {
-        // SAFETY: We know `self.field` was gained via
-        //         `PinnedField::pinned_field_of_mut()`, hence we know the
-        //         entire container is mutably borrowed and pinned.
-        unsafe {
-            PinnedField::pinned_container_of_mut(self.field)
+            field.map_unchecked(|v| &*(
+                (v as *const T)
+                    .byte_offset(-(OFFSET as isize))
+                    .cast::<Self>()
+            ))
         }
     }
 }
@@ -237,28 +55,30 @@ where
 #[cfg(test)]
 mod test {
     use super::*;
+    use crate::mem;
 
-    #[derive(Default)]
+    #[derive(Clone, Copy, Debug, Default, PartialEq)]
     #[repr(C, align(4))]
     struct Test {
-        a: u32,
+        a: u16,
         b: u8,
         c: u32,
     }
 
-    unsafe impl meta::Field<{core::mem::offset_of!(Test, b)}> for Test {
-        type Value = u8;
-    }
-    unsafe impl PinnedField<{core::mem::offset_of!(Test, b)}> for Test {
+    unsafe impl PinnedField<{mem::typed_offset_of!(Test, b, u8)}, u8> for Test {
     }
 
-    // Basic functionality tests for `Field`.
+    // Basic functionality tests for `PinnedField`.
     #[test]
-    fn basic_field() {
-        assert_eq!(core::mem::size_of::<Test>(), 12);
+    fn basic_pinned_field() {
+        assert_eq!(core::mem::size_of::<Test>(), 8);
 
-        let t = pin::pin!(Test { a: 14, b: 11, c: 1444 });
-        let v = t.as_ref().pinned_view();
-        assert_eq!(*v.field(), 11);
+        let v = pin::pin!(Test { a: 14, b: 11, c: 1444 });
+        let c = v.into_ref();
+        let f = PinnedField::pinned_field_of(c);
+
+        assert_eq!(*f, 11);
+        assert!(core::ptr::eq(&*c, unsafe { &*PinnedField::pinned_container_of(f) }));
+        assert_eq!(c, unsafe { PinnedField::pinned_container_of(f) });
     }
 }
