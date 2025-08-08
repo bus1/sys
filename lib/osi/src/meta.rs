606ef40bfc8fe17050fd6398688c67a7e2d74b3b
David Rheinsberg <david@readahead.eu>
2025-08-08 14:00:11 +0200

    osi/meta: rework Field
    Rework the Field trait again. Turn it into a marker trait and simplify
    the helpers around it. Remove all helpers that can easily be misused and
    drop the unused PinField for now.
    
    Note that Stacked Borrows do not allow increasing the object size of a
    reference. Instead, one must always ensure to retain raw pointers when
    intending to reverse offset calculations. This is not an issue for Tree
    Borrows, though. However, we prefer being compatible to Stacked Borrows,
    so lets ensure `Field` is as well.
    
    Signed-off-by: David Rheinsberg <david@readahead.eu>

--- lib/osi/src/meta.rs
+++ lib/osi/src/meta.rs
@@ -9,11 +9,23 @@
 /// access to a specific field without requiring any other knowledge about the
 /// containing type.
 ///
-/// Any type that implements `Field<OFFSET, T>` is guaranteed to have a field
-/// member at relative offset `OFFSET`. The type of the field member is `T`.
+/// Any type that implements `Field<OFFSET, T>` is guaranteed to have a member
+/// field at relative offset `OFFSET`. The type of the member field is `T`.
 ///
 /// Note that multiple fields might share an offset if at least on of them
 /// is a zero-sized type.
+///
+/// ## Dynamically-sized Types (DSTs)
+///
+/// This trait works for dynamically sized types (both for the container as
+/// well as the field). However, the offset of the field in question must be
+/// statically known. Furthermore, in case of dynamically-sized types, offset
+/// calculations will not be able to deduce the size of the container or member
+/// field, and thus cannot create raw pointers or even references (since Rust
+/// uses metadata for DSTs even on raw pointers).
+///
+/// Long story short: If your types are not `Sized`, the provided helpers will
+/// likely not apply.
 ///
 /// ## Safety
 ///
@@ -22,8 +34,7 @@
 /// member field must follow standard alignment and sizing requirements.
 ///
 /// The member field must be a direct member of the structure (rather than
-/// nested in sub-structures). In particular, its type must match exactly the
-/// type `T`.
+/// nested in sub-structures). In particular, its type must match exactly `T`.
 ///
 /// ## Example
 ///
@@ -33,64 +44,114 @@
 /// use osi::meta::Field;
 ///
 /// struct Position {
-///     x: u32,
-///     y: u64,
+///     x: u8,
+///     y: u16,
+///     z: u8,
 /// }
 ///
-/// unsafe impl Field<{typed_offset_of!(Position, x, u32)}, u32> for Position {
+/// unsafe impl Field<{typed_offset_of!(Position, x, u8)}, u8> for Position {
 /// }
 ///
-/// unsafe impl Field<{typed_offset_of!(Position, y, u64)}, u64> for Position {
+/// unsafe impl Field<{typed_offset_of!(Position, y, u16)}, u16> for Position {
 /// }
 ///
-/// let pos = Position { x: 11, y: 1444 };
-/// let pos_x = <_ as Field<{offset_of!{Position, x}}, _>>::field_of(&pos);
-/// let pos_y = <_ as Field<{offset_of!{Position, y}}, _>>::field_of(&pos);
+/// unsafe impl Field<{typed_offset_of!(Position, z, u8)}, u8> for Position {
+/// }
+///
+/// let pos = Position { x: 11, y: 1444, z: 71 };
+/// let pos_x = osi::meta::field_of::<{typed_offset_of!(Position, x, u8)}, u8, _>(&pos);
+/// let pos_y = osi::meta::field_of::<{typed_offset_of!(Position, y, u16)}, u16, _>(&pos);
+/// let pos_z = osi::meta::field_of::<{typed_offset_of!(Position, z, u8)}, u8, _>(&pos);
+///
+/// // With Rust-1.89 and later, you can infer the offset if unambiguous:
+/// // let pos_y = osi::meta::field_of::<_, u16, _>(&pos);
+///
 /// assert_eq!(*pos_x, pos.x);
 /// assert_eq!(*pos_y, pos.y);
+/// assert_eq!(*pos_z, pos.z);
 /// ```
-pub unsafe trait Field<const OFFSET: usize, T>: Sized {
-    /// Turn a container reference into a member field reference.
-    fn field_of(container: &Self) -> &T {
-        // SAFETY: The trait guarantees that there is a valid member field
-        //         at the specified offset. Since we have the entire container
-        //         borrowed, we can safely hand out sub-borrows.
-        unsafe {
-            &*(
-                (container as *const Self)
-                    .byte_offset(OFFSET as isize)
-                    .cast::<T>()
-            )
-        }
-    }
+pub unsafe trait Field<const OFFSET: usize, T: ?Sized> {
+}
 
-    /// Turn a member field reference back into a container reference.
-    ///
-    /// ## Safety
-    ///
-    /// The caller must guarantee that `field` points to a member field of a
-    /// container of type `Self`. Furthermore, the entire container must be
-    /// borrowed for at least the lifetime of `field`.
-    ///
-    /// Note that these guarantees are always given if `field` was acquired
-    /// via `Self::field_of()`.
-    unsafe fn container_of(field: &T) -> &Self {
-        // SAFETY: The caller must guarantee that `field` was borrowed
-        //         together with the entire container.
-        unsafe {
-            &*(
-                (field as *const T)
-                    .byte_offset(-(OFFSET as isize))
-                    .cast::<Self>()
-            )
-        }
-    }
+/// Turn a container pointer into a member field pointer.
+///
+/// This is equivalent to taking a raw pointer to a member field
+/// `&raw const (*container).field`. Note that the container is not
+/// dereferenced for this operation, since this merely needs a place
+/// expression.
+pub fn field_of_ptr<const OFFSET: usize, F, C>(v: *const C) -> *const F
+where
+    C: ?Sized + Field<OFFSET, F>,
+{
+    v.wrapping_byte_offset(OFFSET as isize).cast()
+}
+
+/// Turn a container reference into a member field reference.
+///
+/// This is equivalent to taking a reference to a member field
+/// `&container.field`. However, this function will always borrow the entire
+/// container for as long as the field is borrowed (i.e., you cannot split
+/// borrows across multiple fields).
+///
+/// This function uses the [`Field`] trait internally.
+pub fn field_of<const OFFSET: usize, F, C>(v: &C) -> &F
+where
+    C: ?Sized + Field<OFFSET, F>,
+{
+    // SAFETY: `Field` guarantees that there is a valid member field at the
+    //         specified offset. Hence, `field_of_ptr()` cannot wrap around nor
+    //         leave the allocation.
+    //         Since we have the entire container borrowed, we can safely hand
+    //         out sub-borrows.
+    unsafe { &*field_of_ptr(v) }
+}
+
+/// Turn a mutable container reference into a mutable member field reference.
+///
+/// This is equivalent to taking a mutable reference to a member field
+/// `&mut container.field`. However, this function will always mutably borrow
+/// the entire container for as long as the field is borrowed (i.e., you cannot
+/// split borrows across multiple fields).
+///
+/// This function uses the [`Field`] trait internally.
+pub fn field_of_mut<const OFFSET: usize, F, C>(v: &mut C) -> &mut F
+where
+    C: ?Sized + Field<OFFSET, F>,
+{
+    // SAFETY: `Field` guarantees that there is a valid member field at the
+    //         specified offset. Hence, `field_of_ptr()` cannot wrap around nor
+    //         leave the allocation.
+    //         Since we have the entire container borrowed, we can safely hand
+    //         out sub-borrows.
+    unsafe { &mut *(field_of_ptr(v) as *mut _) }
+}
+
+/// Turn a field pointer into a container pointer
+///
+/// This is the inverse of [`field_of_ptr()`]. It recreates the container
+/// pointer from the member field pointer.
+///
+/// ## Miri Stacked & Tree Borrows
+///
+/// If you require compatibility with Stacked Borrows as used in Miri, you must
+/// ensure that the field pointer was created from a reference to the
+/// container, rather than from a reference to the field. In other words, make
+/// sure that you use [`field_of_ptr()`], rather than [`field_of()`], and then
+/// retain that raw field pointer until you need it for [`container_of_ptr()`].
+/// Otherwise, your code will likely not be compatible with Stacked Borrows.
+///
+/// If you only require compatibility with Tree Borrows, this is not an issue.
+pub fn container_of_ptr<const OFFSET: usize, F, C>(v: *const F) -> *const C
+where
+    C: Field<OFFSET, F>,
+    F: ?Sized,
+{
+    v.wrapping_byte_offset(-(OFFSET as isize)).cast()
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-    use crate::mem;
 
     #[derive(Clone, Copy, Debug, Default, PartialEq)]
     #[repr(C, align(4))]
@@ -100,7 +161,7 @@ mod test {
         c: u32,
     }
 
-    unsafe impl Field<{mem::typed_offset_of!(Test, b, u8)}, u8> for Test {
+    unsafe impl Field<{crate::mem::typed_offset_of!(Test, b, u8)}, u8> for Test {
     }
 
     // Basic functionality tests for `Field`.
@@ -108,11 +169,19 @@ mod test {
     fn basic_field() {
         assert_eq!(core::mem::size_of::<Test>(), 8);
 
-        let c = Test { a: 14, b: 11, c: 1444 };
-        let f = Field::field_of(&c);
+        let o = Test { a: 14, b: 11, c: 1444 };
+        let o_p = &raw const o;
 
+        let f = field_of(&o);
         assert_eq!(*f, 11);
-        assert!(core::ptr::eq(&c, unsafe { Field::container_of(f) }));
-        assert_eq!(c, unsafe { *Field::container_of(f) });
+
+        let f_p = field_of_ptr(o_p);
+        let f_r = unsafe { &*f_p };
+        let c_p = container_of_ptr(f_p);
+        let c_r = unsafe { &*c_p };
+
+        assert!(core::ptr::eq(o_p, c_p));
+        assert_eq!(*f_r, 11);
+        assert_eq!(c_r.b, 11);
     }
 }
--- lib/osi/src/pin.rs
+++ lib/osi/src/pin.rs
@@ -43,53 +43,6 @@ where
     unsafe { pin::Pin::new_unchecked(f(pin::Pin::into_inner_unchecked(v))) }
 }
 
-/// Grant generic access to pinned member fields.
-///
-/// This trait is a pinned alternative to [`crate::meta::Field`].
-///
-/// ## Safety
-///
-/// Any implementation must uphold the requirements of [`crate::meta::Field`].
-/// On top, the member field at offset `OFFSET` must follow the rules of
-/// `structural pinning` as described in [`core::pin::Pin`]
-pub unsafe trait PinnedField<const OFFSET: usize, T>: Sized {
-    /// Turn a pinned container reference into a pinned member field reference.
-    fn pinned_field_of(container: pin::Pin<&Self>) -> pin::Pin<&T> {
-        // SAFETY: The trait guarantees structural pinning for the member
-        //         field. Therefore, we can safely pin the field.
-        unsafe {
-            container.map_unchecked(|v| &*(
-                (v as *const Self)
-                    .byte_offset(OFFSET as isize)
-                    .cast::<T>()
-            ))
-        }
-    }
-
-    /// Turn a pinned member field reference back into a pinned container
-    /// reference.
-    ///
-    /// ## Safety
-    ///
-    /// The caller must guarantee that `field` points to a member field of a
-    /// container of type `Self`. Furthermore, the entire container must be
-    /// borrowed and pinned for at least the lifetime of `field`.
-    ///
-    /// Note that these guarantees are always given if `field` was acquired
-    /// via `Self::pinned_field_of()`.
-    unsafe fn pinned_container_of(field: pin::Pin<&T>) -> pin::Pin<&Self> {
-        // SAFETY: The caller guarantees that the entire container is borrowed
-        //         and pinned.
-        unsafe {
-            field.map_unchecked(|v| &*(
-                (v as *const T)
-                    .byte_offset(-(OFFSET as isize))
-                    .cast::<Self>()
-            ))
-        }
-    }
-}
-
 #[cfg(test)]
 mod test {
     use super::*;
@@ -120,30 +73,5 @@ mod test {
             map_unchecked(r1, |v: &u16| -> &[u8; 2] { &*(v as *const u16 as *const _) })
         };
         assert!(r2[0] == 71 || r2[1] == 71);
-    }
-
-    #[derive(Clone, Copy, Debug, Default, PartialEq)]
-    #[repr(C, align(4))]
-    struct Test {
-        a: u16,
-        b: u8,
-        c: u32,
-    }
-
-    unsafe impl PinnedField<{crate::mem::typed_offset_of!(Test, b, u8)}, u8> for Test {
-    }
-
-    // Basic functionality tests for `PinnedField`.
-    #[test]
-    fn basic_pinned_field() {
-        assert_eq!(core::mem::size_of::<Test>(), 8);
-
-        let v = pin::pin!(Test { a: 14, b: 11, c: 1444 });
-        let c = v.into_ref();
-        let f = PinnedField::pinned_field_of(c);
-
-        assert_eq!(*f, 11);
-        assert!(core::ptr::eq(&*c, unsafe { &*PinnedField::pinned_container_of(f) }));
-        assert_eq!(c, unsafe { PinnedField::pinned_container_of(f) });
     }
 }
