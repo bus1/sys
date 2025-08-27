5aa9b29a5c48fd25c72e6318ce4ac46b37684fce
David Rheinsberg <david@readahead.eu>
2025-08-27 10:19:57 +0200

    osi/convert: use OnceRef over DerefPtr
    We now have a generic replacement for DerefPtr, so make use of it.
    
    Signed-off-by: David Rheinsberg <david@readahead.eu>

--- lib/osi/src/convert.rs
+++ lib/osi/src/convert.rs
@@ -4,6 +4,7 @@
 //! types.
 
 use core::pin;
+use crate::ptr::OnceRef;
 
 /// Convert a value into a wrapped pointer to its dereferenced value.
 ///
@@ -18,13 +19,18 @@ use core::pin;
 /// steps are taken. Usually, you want to restore the original value to ensure
 /// the correct drop handlers are run (see [`FromDeref`]).
 ///
+/// This trait is a generic version of
+/// [`Box::into_raw()`](alloc::boxed::Box::into_raw),
+/// [`Rc::into_raw()`](alloc::rc::Rc::into_raw),
+/// [`Arc::into_raw()`](alloc::sync::Arc::into_raw), and more.
+///
 /// ## Mutability
 ///
 /// [`IntoDeref`] serves for immutable and mutable conversions. The returned
 /// wrapper type properly wraps both mutable and immutable references. It never
 /// hands out safe mutable access, but also prevents safe creation of copies.
 ///
-/// See [`DerefPtr`] for details.
+/// See [`OnceRef`] for details.
 ///
 /// ## Safety
 ///
@@ -46,19 +52,21 @@ pub unsafe trait IntoDeref: Sized + core::ops::Deref {
     /// While the returned wrapper can be treated as a `&'a Self::Target`, the
     /// wrapper provides additional guarantees that the reference cannot. See
     /// its definition for details.
-    fn into_deref<'a>(v: Self) -> DerefPtr<'a, Self::Target> where Self: 'a;
+    fn into_deref<'a>(v: Self) -> OnceRef<'a, Self::Target> where Self: 'a;
 
     /// Convert a pinned value into a wrapped pointer to its dereferenced
     /// value.
     ///
     /// This is the pinned equivalent of [`Self::into_deref()`].
-    fn pin_into_deref<'a>(v: pin::Pin<Self>) -> DerefPtr<'a, Self::Target>
+    fn pin_into_deref<'a>(v: pin::Pin<Self>) -> pin::Pin<OnceRef<'a, Self::Target>>
     where
         pin::Pin<Self>: 'a,
     {
         // SAFETY: Pinned types must ensure they uphold pinning guarantees
         //         just like `Deref` does (see trait requirements).
-        Self::into_deref(unsafe { pin::Pin::into_inner_unchecked(v) })
+        unsafe {
+            crate::pin::map_unchecked(v, Self::into_deref)
+        }
     }
 }
 
@@ -66,9 +74,14 @@ pub unsafe trait IntoDeref: Sized + core::ops::Deref {
 /// value.
 ///
 /// This trait provides the inverse operation of [`IntoDeref`]. It takes a
-/// pointer to a dereferenced value and restores the original smart pointer.
+/// pointer to a dereferenced value and restores the original pointer.
 /// This operation is unsafe and requires the caller to guarantee that the
 /// pointer was acquired via [`IntoDeref`] or similar means.
+///
+/// This trait is a generic version of
+/// [`Box::from_raw()`](alloc::boxed::Box::from_raw),
+/// [`Rc::from_raw()`](alloc::rc::Rc::from_raw),
+/// [`Arc::from_raw()`](alloc::sync::Arc::from_raw), and more.
 ///
 /// ## Safety
 ///
@@ -97,7 +110,7 @@ pub unsafe trait FromDeref: Sized + core::ops::Deref {
     /// wrapped pointer (note that such copies cannot be created in safe code).
     ///
     /// It is always safe to call this on values obtained via [`IntoDeref`].
-    unsafe fn from_deref<'a>(v: DerefPtr<'a, Self::Target>) -> Self;
+    unsafe fn from_deref<'a>(v: OnceRef<'a, Self::Target>) -> Self;
 
     /// Convert a wrapped pointer to a dereferenced value back to its original
     /// pinned value.
@@ -108,119 +121,15 @@ pub unsafe trait FromDeref: Sized + core::ops::Deref {
     ///
     /// The caller must guarantee that the original value was a pinned pointer.
     /// Furthermore, all requirements of [`Self::from_deref()`] apply.
-    unsafe fn pin_from_deref<'a>(v: DerefPtr<'a, Self::Target>) -> pin::Pin<Self> {
+    unsafe fn pin_from_deref<'a>(v: pin::Pin<OnceRef<'a, Self::Target>>) -> pin::Pin<Self> {
         // SAFETY: Pinned types must ensure they uphold pinning guarantees
         //         just like `Deref` does (see trait requirements). Also, the
         //         caller must ensure the original value was pinned.
-        unsafe { core::pin::Pin::new_unchecked(Self::from_deref(v)) }
+        unsafe { crate::pin::map_unchecked(v, |v| Self::from_deref(v)) }
     }
 }
 
-/// A wrapped pointer to a dereferenced value.
-///
-/// In and of itself, this type is a wrapper around a
-/// [`core::ptr::NonNull<T>`] for a fixed lifetime `'a`. It can be safely
-/// dereferenced to a `&'a T`. However, this type is not equivalent to a mere
-/// reference. In particular, it differs in the following ways:
-///
-///  1) It cannot be cloned or copied. This ensures that if it was
-///     created from a mutable reference, no copies of it can be created.
-///  2) It is always rooted in a raw pointer. That is, any
-///     reference it hands out is always temporary and cannot outlive it. This
-///     allows better compatibility with Miri Stacked Borrows and Tree Borrows.
-///
-/// [`DerefPtr`] can be used on its own, but note that it partakes in
-/// [`IntoDeref`] and [`FromDeref`] and is designed exactly for those traits.
-/// Hence, it might have unsuitable restrictions for other use-cases.
-#[derive(Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
-#[repr(transparent)]
-pub struct DerefPtr<'a, T: ?Sized> {
-    ptr: core::ptr::NonNull<T>,
-    _ref: core::marker::PhantomData<&'a T>,
-}
-
-unsafe impl<'a, T: ?Sized + Sync> Send for DerefPtr<'a, T> {
-}
-
-unsafe impl<'a, T: ?Sized + Sync> Sync for DerefPtr<'a, T> {
-}
-
-impl<'a, T: ?Sized> DerefPtr<'a, T> {
-    unsafe fn new(v: core::ptr::NonNull<T>) -> Self {
-        Self {
-            ptr: v,
-            _ref: Default::default(),
-        }
-    }
-
-    /// Create a new instance from a [`NonNull`](core::ptr::NonNull).
-    ///
-    /// ## Safety
-    ///
-    /// The non-null pointer must pointer to a valid object of type `T`, and it
-    /// must be valid for at least lifetime `'a`.
-    ///
-    /// No mutable reference to the object must be live for at least `'a`. That
-    /// is, it must be safe for this wrapper to immutably borrow the object for
-    /// `'a`.
-    pub unsafe fn from_nonnull(v: core::ptr::NonNull<T>) -> Self {
-        unsafe { Self::new(v) }
-    }
-
-    /// Create a new instance from a raw pointer.
-    ///
-    /// ## Safety
-    ///
-    /// See [`Self::from_nonnull()`].
-    pub unsafe fn from_ptr(v: *const T) -> Self {
-        unsafe { Self::new(core::ptr::NonNull::new_unchecked(v as *mut _)) }
-    }
-
-    /// Create a new instance from a reference.
-    pub fn from_ref(v: &'a T) -> Self {
-        unsafe { Self::new(crate::ptr::nonnull_from_ref(v)) }
-    }
-
-    /// Create a new instance from a mutable reference.
-    pub fn from_mut(v: &'a mut T) -> Self {
-        unsafe { Self::new(crate::ptr::nonnull_from_mut(v)) }
-    }
-
-    /// Convert this into its underlying [`NonNull`](core::ptr::NonNull).
-    pub fn into_nonnull(self) -> core::ptr::NonNull<T> {
-        self.ptr
-    }
-
-    /// Convert this into its underlying raw pointer.
-    pub fn into_ptr(self) -> *const T {
-        self.ptr.as_ptr()
-    }
-
-    /// Borrow the underlying [`NonNull`](core::ptr::NonNull).
-    pub fn as_nonnull(&self) -> core::ptr::NonNull<T> {
-        self.ptr
-    }
-
-    /// Borrow the underlying raw pointer.
-    pub fn as_ptr(&self) -> *const T {
-        self.ptr.as_ptr()
-    }
-
-    /// Dereference this wrapper to the pointed object.
-    pub fn deref(&self) -> &T {
-        unsafe { self.ptr.as_ref() }
-    }
-}
-
-impl<'a, T: ?Sized> core::ops::Deref for DerefPtr<'a, T> {
-    type Target = T;
-
-    fn deref(&self) -> &Self::Target {
-        self.deref()
-    }
-}
-
-impl<'a, T> From<T> for DerefPtr<'a, T::Target>
+impl<'a, T> From<T> for OnceRef<'a, T::Target>
 where
     T: 'a + core::ops::Deref + IntoDeref,
 {
@@ -229,15 +138,12 @@ where
     }
 }
 
-impl<'a, T: ?Sized> From<DerefPtr<'a, T>> for core::ptr::NonNull<T> {
-    fn from(v: DerefPtr<'a, T>) -> Self {
-        v.into_nonnull()
-    }
-}
-
-impl<'a, T: ?Sized> From<DerefPtr<'a, T>> for *const T {
-    fn from(v: DerefPtr<'a, T>) -> Self {
-        v.into_ptr()
+impl<'a, T> From<pin::Pin<T>> for pin::Pin<OnceRef<'a, T::Target>>
+where
+    T: 'a + core::ops::Deref + IntoDeref,
+{
+    fn from(v: pin::Pin<T>) -> Self {
+        IntoDeref::pin_into_deref(v)
     }
 }
 
@@ -246,65 +152,65 @@ mod lib {
     use alloc::{boxed::Box, rc::Rc, sync::Arc};
 
     unsafe impl<T: ?Sized> IntoDeref for &T {
-        fn into_deref<'a>(v: Self) -> DerefPtr<'a, Self::Target> where Self: 'a {
-            DerefPtr::from_ref(v)
+        fn into_deref<'a>(v: Self) -> OnceRef<'a, Self::Target> where Self: 'a {
+            OnceRef::from_ref(v)
         }
     }
 
     unsafe impl<T: ?Sized> FromDeref for &T {
-        unsafe fn from_deref<'a>(v: DerefPtr<'a, Self::Target>) -> Self {
+        unsafe fn from_deref<'a>(v: OnceRef<'a, Self::Target>) -> Self {
             unsafe { v.into_nonnull().as_ref() }
         }
     }
 
     unsafe impl<T: ?Sized> IntoDeref for &mut T {
-        fn into_deref<'a>(v: Self) -> DerefPtr<'a, Self::Target> where Self: 'a {
-            DerefPtr::from_mut(v)
+        fn into_deref<'a>(v: Self) -> OnceRef<'a, Self::Target> where Self: 'a {
+            OnceRef::from_mut(v)
         }
     }
 
     unsafe impl<T: ?Sized> FromDeref for &mut T {
-        unsafe fn from_deref<'a>(v: DerefPtr<'a, Self::Target>) -> Self {
+        unsafe fn from_deref<'a>(v: OnceRef<'a, Self::Target>) -> Self {
             unsafe { v.into_nonnull().as_mut() }
         }
     }
 
     unsafe impl<T: ?Sized> IntoDeref for Box<T> {
-        fn into_deref<'a>(v: Self) -> DerefPtr<'a, Self::Target> where Self: 'a {
+        fn into_deref<'a>(v: Self) -> OnceRef<'a, Self::Target> where Self: 'a {
             unsafe {
-                DerefPtr::from_ptr(Box::into_raw(v))
+                OnceRef::from_ptr(Box::into_raw(v))
             }
         }
     }
 
     unsafe impl<T: ?Sized> FromDeref for Box<T> {
-        unsafe fn from_deref<'a>(v: DerefPtr<'a, Self::Target>) -> Self {
+        unsafe fn from_deref<'a>(v: OnceRef<'a, Self::Target>) -> Self {
             unsafe { Box::from_raw(v.into_nonnull().as_ptr()) }
         }
     }
 
     unsafe impl<T: ?Sized> IntoDeref for Rc<T> {
-        fn into_deref<'a>(v: Self) -> DerefPtr<'a, Self::Target> where Self: 'a {
+        fn into_deref<'a>(v: Self) -> OnceRef<'a, Self::Target> where Self: 'a {
             unsafe {
-                DerefPtr::from_ptr(Rc::into_raw(v))
+                OnceRef::from_ptr(Rc::into_raw(v) as *mut _)
             }
         }
     }
 
     unsafe impl<T: ?Sized> FromDeref for Rc<T> {
-        unsafe fn from_deref<'a>(v: DerefPtr<'a, Self::Target>) -> Self {
+        unsafe fn from_deref<'a>(v: OnceRef<'a, Self::Target>) -> Self {
             unsafe { Rc::from_raw(v.into_nonnull().as_ptr()) }
         }
     }
 
     unsafe impl<T: ?Sized> IntoDeref for Arc<T> {
-        fn into_deref<'a>(v: Self) -> DerefPtr<'a, Self::Target> where Self: 'a {
-            unsafe { DerefPtr::from_ptr(Arc::into_raw(v)) }
+        fn into_deref<'a>(v: Self) -> OnceRef<'a, Self::Target> where Self: 'a {
+            unsafe { OnceRef::from_ptr(Arc::into_raw(v) as *mut _) }
         }
     }
 
     unsafe impl<T: ?Sized> FromDeref for Arc<T> {
-        unsafe fn from_deref<'a>(v: DerefPtr<'a, Self::Target>) -> Self {
+        unsafe fn from_deref<'a>(v: OnceRef<'a, Self::Target>) -> Self {
             unsafe { Arc::from_raw(v.into_nonnull().as_ptr()) }
         }
     }
@@ -323,8 +229,8 @@ mod test {
             let p: *const u64 = &raw const v;
             let f: &u64 = &v;
 
-            let d: DerefPtr<u64> = IntoDeref::into_deref(f);
-            assert_eq!(71, *d.deref());
+            let d: OnceRef<u64> = IntoDeref::into_deref(f);
+            assert_eq!(71, *d.as_ref());
             assert!(core::ptr::eq(p, d.as_ptr()));
 
             let r: &u64 = unsafe { FromDeref::from_deref(d) };
@@ -336,8 +242,8 @@ mod test {
             let p: *mut u64 = &raw mut v;
             let f: &mut u64 = &mut v;
 
-            let d: DerefPtr<u64> = IntoDeref::into_deref(f);
-            assert_eq!(71, *d.deref());
+            let d: OnceRef<u64> = IntoDeref::into_deref(f);
+            assert_eq!(71, *d.as_ref());
             assert!(core::ptr::eq(p, d.as_ptr()));
 
             let r: &mut u64 = unsafe { FromDeref::from_deref(d) };
@@ -349,7 +255,7 @@ mod test {
             let f: Box<u64> = Box::new(v);
             let p: *const u64 = &raw const *f;
 
-            let d: DerefPtr<u64> = f.into();
+            let d: OnceRef<u64> = f.into();
             assert_eq!(71, *d);
             assert!(core::ptr::eq(p, d.as_ptr()));
 
@@ -362,7 +268,7 @@ mod test {
             let f: Rc<u64> = Rc::new(v);
             let p: *const u64 = &raw const *f;
 
-            let d: DerefPtr<u64> = f.into();
+            let d: OnceRef<u64> = f.into();
             assert_eq!(71, *d);
             assert!(core::ptr::eq(p, d.as_ptr()));
 
@@ -375,7 +281,7 @@ mod test {
             let f: Arc<u64> = Arc::new(v);
             let p: *const u64 = &raw const *f;
 
-            let d: DerefPtr<u64> = f.into();
+            let d: OnceRef<u64> = f.into();
             assert_eq!(71, *d);
             assert!(core::ptr::eq(p, d.as_ptr()));
 
