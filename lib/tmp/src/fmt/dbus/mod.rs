c973adc1eed579044408bda00fe5530fb77e7eac
David Rheinsberg <david@readahead.eu>
2025-12-12 20:23:59 +0100

    tmp/fmt/dbus: add encoding/decoding layers
    Add encoding and decoding layers for the D-Bus formatting module.
    
    This uses the new:
    
    - D-Bus type-system abstraction available in `tmp::fmt::dbus`
    - I/O abstractions for streaming data and memory mapped data in
      `tmp::io`.
    - Streaming JSON implementation in `tmp::fmt::json`
    
    This is modeled after `c-dvar`, the C implementation of D-Bus Variants
    as provided by dbus-broker.
    
    Signed-off-by: David Rheinsberg <david@readahead.eu>

--- lib/tmp/src/fmt/dbus/mod.rs
+++ lib/tmp/src/fmt/dbus/mod.rs
@@ -1,7 +1,26 @@
 //! # D-Bus Variants
+//!
+//! XXX
 
+pub mod dvar;
 pub mod element;
+pub mod ende;
 pub mod signature;
 
 pub use element::Element;
 pub use signature::{Cursor, Sig, sig};
+
+#[derive(Clone, Copy, Debug, Hash)]
+#[derive(Eq, Ord, PartialEq, PartialOrd)]
+pub enum Error {
+    /// The underlying I/O operation failed.
+    Io(crate::io::map::Error),
+    /// The provided type does not match the signature.
+    Mismatch,
+    /// The signature has not been fully processed.
+    Pending,
+    /// The passed data overflows the supported length of the encoding.
+    DataOverflow,
+    /// The data is not valid UTF-8.
+    DataNonUtf8,
+}
